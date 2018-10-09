1b5d3acafebc633d86028ea3017c6949db7cf37e
Sharmaine Requerme <srqrme@gmail.com>
2018-10-09 21:02:13 +0000

    Added and modified file- lib/moviesOnNetflix/cli.rb

--- lib/moviesOnNetflix.rb
+++ lib/moviesOnNetflix.rb
@@ -1,4 +1,5 @@
-require "moviesOnNetflix/version"
+require_relative "./moviesOnNetflix/version"
+require_relative "./moviesOnNetflix/cli"
 
 module MoviesOnNetflix
   # Your code goes here...
---
+++ lib/moviesOnNetflix/cli.rb
@@ -0,0 +1,9 @@
+# CLI Controller
+
+class MoviesOnNetflix::CLI
+
+  def call
+    puts "Hello and Welcome to Rotten Tomatoes Guide to the best Netflix movies by Tomatometer!"
+  end
+
+end
