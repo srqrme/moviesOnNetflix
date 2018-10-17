655da1bb224ad9b403245c63f8cf1338df26e9e2
Sharmaine Requerme <srqrme@gmail.com>
2018-10-17 23:10:11 +0000

    modified files

--- lib/moviesOnNetflix/cli.rb
+++ lib/moviesOnNetflix/cli.rb
@@ -10,16 +10,14 @@ class MoviesOnNetflix::CLI
   def menu
     puts "Please enter a number from the following options"
     puts "1. List Movies"
-    puts "2. How To"
-    puts "3. Quit"
+    puts "2. Quit"
     input = gets.strip.to_i
 
     case input
       when 1
+        puts "Best Netflix Movies To Watch Right Now, Listed By Rank:"
         list_movies
       when 2
-        how_to
-      when 3
         puts "Thank you for using moviesOnNetflix, have a nice day!"
         exit
       else
@@ -28,27 +26,10 @@ class MoviesOnNetflix::CLI
     end
   end
 
-  def how_to
-    puts "How to use moviesOnNetflix:"
-    puts "In the welcome page, enter 1 to list all movies. Each movie will be listed by rank according to Rotten Tomatoes Tomato Meter."
-    puts "Enter 2 to go to this instructional page"
-    puts "Enter 3 to quit moviesOnNetflix"
-    puts "Would you like to go back to the welcome page?"
-    input = gets.strip.downcase
-    if input == "y"
-      menu
-    elsif input == "n"
-      puts "Thank you for using moviesOnNetflix, have a nice day!"
-    else
-      puts "I'm not sure I understand what you want to do."
-      menu
-    end
-  end
   def list_movies
     MoviesOnNetflix::Movie.create_from_collection
     MoviesOnNetflix::Movie.all.each do |movie|
-      puts "Rank:    #{movie.rank}"
-      puts "Title:   #{movie.title}"
+      puts "#{movie.rank}   #{movie.title}"
     end
     puts "Please enter the rank number of the movie you'd like more information for:"
     input = gets.strip.to_i
